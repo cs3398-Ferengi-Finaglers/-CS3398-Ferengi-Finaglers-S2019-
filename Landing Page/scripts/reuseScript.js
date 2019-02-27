af8b37ef3a48b0e1801f25a3eb343ad750fad0be
Jrs389 <46977649+Jrs389@users.noreply.github.com>
2019-02-27 16:31:42 -0600

    Add files via upload

---
+++ Landing Page/scripts/reuseScript.js
@@ -0,0 +1,6 @@
+$(function() {
+	$('#reuseHeader').load('/fuelrate/reuseHTML/header.html');
+	$('#reuseNavbar').load('/fuelrate/reuseHTML/navbar.html');
+	$('#reuseFooterWhite').load('/fuelrate/reuseHTML/footerWhite.html');
+	$('#reuseFooterBlack').load('/fuelrate/reuseHTML/footerBlack.html');
+});
