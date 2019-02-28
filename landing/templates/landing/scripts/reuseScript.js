7ac93c955815dca91d3f2a31d8ca0dbfa594cfdc
KProft <kcp36@txstate.edu>
2019-02-28 01:20:57 -0600

    Landing page now displays properly

--- landing/templates/landing/scripts/reuseScript.js
+++
@@ -1,6 +0,0 @@
-$(function() {
-	$('#reuseHeader').load('/fuelrate/reuseHTML/header.html');
-	$('#reuseNavbar').load('/fuelrate/reuseHTML/navbar.html');
-	$('#reuseFooterWhite').load('/fuelrate/reuseHTML/footerWhite.html');
-	$('#reuseFooterBlack').load('/fuelrate/reuseHTML/footerBlack.html');
-});
---
+++ static/scripts/reuseScript.js
@@ -0,0 +1,6 @@
+$(function() {
+	$('#reuseHeader').load('static/reuseHTML/header.html');
+	$('#reuseNavbar').load('static/reuseHTML/navbar.html');
+	$('#reuseFooterWhite').load('static/reuseHTML/footerWhite.html');
+	$('#reuseFooterBlack').load('static/reuseHTML/footerBlack.html');
+});
