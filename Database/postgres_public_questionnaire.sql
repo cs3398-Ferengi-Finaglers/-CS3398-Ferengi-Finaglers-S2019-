3728c2cbf1159a874dd43feabadeaa0e8abfee9c
APRYM <37633613+APRYM@users.noreply.github.com>
2019-03-05 15:15:34 -0600

    Update postgres_public_questionnaire.sql

--- Database/postgres_public_questionnaire.sql
+++ Database/postgres_public_questionnaire.sql
@@ -1,15 +1,66 @@
-CREATE TABLE public.questionnaire
+create table public.questionnaire
 (
-    user_id integer DEFAULT nextval('questionnaire_used_id_seq'::regclass) PRIMARY KEY NOT NULL,
-    question1 char NOT NULL,
-    question2 char NOT NULL,
-    question3 char NOT NULL,
-    question4 char NOT NULL,
-    question5 char NOT NULL,
-    question6 char NOT NULL,
-    question7 char NOT NULL,
-    question8 char NOT NULL,
-    question9 char NOT NULL,
-    question10 char NOT NULL
+  user_id    integer not null
+    constraint questionnaire_pkey
+      primary key,
+  question1  char    not null,
+  question2  char    not null,
+  question3  char    not null,
+  question4  char    not null,
+  question5  char    not null,
+  question6  char    not null,
+  question7  char    not null,
+  question8  char    not null,
+  question9  char    not null,
+  question10 char    not null
 );
 INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (1, 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A', 'A');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (2, 'B', 'C', 'D', 'C', 'C', 'C', 'B', 'D', 'D', 'D');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (3, 'C', 'B', 'A', 'A', 'A', 'A', 'C', 'C', 'C', 'C');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (4, 'D', 'A', 'B', 'D', 'D', 'D', 'D', 'B', 'B', 'B');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (5, 'A', 'D', 'C', 'A', 'C', 'B', 'A', 'A', 'A', 'A');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (6, 'B', 'C', 'D', 'C', 'A', 'A', 'B', 'D', 'D', 'D');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (7, 'C', 'B', 'A', 'A', 'D', 'D', 'C', 'C', 'C', 'C');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (8, 'D', 'A', 'C', 'D', 'C', 'C', 'D', 'D', 'B', 'B');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (9, 'A', 'D', 'B', 'A', 'A', 'B', 'C', 'C', 'A', 'A');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (10, 'B', 'C', 'A', 'D', 'D', 'A', 'B', 'B', 'D', 'D');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (11, 'C', 'B', 'D', 'C', 'B', 'D', 'A', 'A', 'B', 'B');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (12, 'D', 'A', 'C', 'B', 'A', 'C', 'D', 'D', 'D', 'C');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (13, 'A', 'D', 'B', 'A', 'D', 'B', 'C', 'C', 'A', 'D');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (14, 'B', 'C', 'A', 'D', 'C', 'A', 'B', 'B', 'B', 'A');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (15, 'C', 'B', 'D', 'C', 'B', 'D', 'C', 'A', 'C', 'B');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (16, 'D', 'A', 'C', 'B', 'A', 'C', 'D', 'D', 'D', 'C');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (17, 'A', 'D', 'B', 'A', 'D', 'B', 'A', 'B', 'A', 'D');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (18, 'B', 'C', 'A', 'D', 'C', 'A', 'B', 'D', 'B', 'A');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (19, 'C', 'B', 'D', 'C', 'B', 'D', 'C', 'C', 'C', 'B');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (20, 'D', 'A', 'C', 'B', 'A', 'C', 'D', 'B', 'D', 'C');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (21, 'A', 'D', 'B', 'A', 'D', 'B', 'A', 'A', 'A', 'D');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (22, 'B', 'C', 'A', 'D', 'C', 'A', 'B', 'D', 'B', 'A');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (23, 'C', 'B', 'D', 'C', 'B', 'D', 'C', 'C', 'C', 'B');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (24, 'D', 'A', 'C', 'B', 'A', 'C', 'D', 'B', 'D', 'C');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (25, 'A', 'D', 'B', 'A', 'D', 'B', 'A', 'A', 'A', 'D');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (26, 'B', 'C', 'A', 'D', 'C', 'A', 'B', 'D', 'B', 'A');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (27, 'C', 'B', 'D', 'B', 'B', 'D', 'C', 'B', 'C', 'B');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (28, 'D', 'A', 'C', 'C', 'A', 'B', 'D', 'D', 'D', 'C');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (29, 'A', 'D', 'B', 'A', 'D', 'C', 'A', 'B', 'A', 'D');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (30, 'B', 'C', 'A', 'D', 'C', 'A', 'B', 'D', 'B', 'A');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (31, 'C', 'B', 'D', 'C', 'B', 'D', 'C', 'C', 'C', 'B');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (32, 'D', 'A', 'C', 'B', 'A', 'C', 'D', 'B', 'D', 'C');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (33, 'A', 'D', 'B', 'C', 'D', 'B', 'A', 'A', 'A', 'D');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (34, 'B', 'C', 'A', 'A', 'B', 'C', 'B', 'D', 'B', 'A');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (35, 'C', 'B', 'D', 'D', 'C', 'A', 'C', 'C', 'C', 'B');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (36, 'D', 'A', 'B', 'C', 'A', 'D', 'D', 'B', 'D', 'C');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (37, 'A', 'D', 'C', 'C', 'D', 'C', 'A', 'A', 'A', 'D');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (38, 'B', 'B', 'A', 'A', 'C', 'A', 'B', 'D', 'B', 'A');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (39, 'C', 'C', 'D', 'D', 'B', 'D', 'C', 'B', 'C', 'B');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (40, 'D', 'A', 'C', 'C', 'A', 'C', 'D', 'C', 'D', 'C');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (41, 'A', 'D', 'B', 'A', 'C', 'B', 'A', 'D', 'A', 'D');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (42, 'B', 'C', 'A', 'D', 'B', 'A', 'B', 'A', 'B', 'A');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (43, 'C', 'B', 'D', 'D', 'A', 'D', 'A', 'B', 'C', 'B');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (44, 'D', 'A', 'C', 'C', 'D', 'C', 'B', 'C', 'D', 'D');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (45, 'A', 'D', 'B', 'D', 'C', 'B', 'C', 'D', 'A', 'C');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (46, 'B', 'C', 'A', 'A', 'B', 'A', 'D', 'A', 'B', 'B');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (47, 'C', 'B', 'D', 'B', 'A', 'D', 'A', 'B', 'C', 'A');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (48, 'D', 'A', 'C', 'C', 'D', 'C', 'B', 'C', 'D', 'D');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (49, 'A', 'D', 'D', 'D', 'C', 'B', 'C', 'D', 'A', 'C');
+INSERT INTO public.questionnaire (user_id, question1, question2, question3, question4, question5, question6, question7, question8, question9, question10) VALUES (50, 'B', 'C', 'A', 'A', 'B', 'A', 'D', 'A', 'B', 'B');
