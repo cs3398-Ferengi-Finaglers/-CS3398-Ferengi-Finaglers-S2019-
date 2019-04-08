c1ca5a9732286129ff57e506347da8659f73eea6
Jrs389 <46977649+Jrs389@users.noreply.github.com>
2019-04-07 20:29:26 -0500

    Update views.py
    This connects the registration page to the questionnaire, we must find a way to auto login to do this.

--- registration/views.py
+++ registration/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import UserRegisterForm
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.views import LoginView 
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponseRedirect
+
 
 
 def register(request):
@@ -11,14 +13,15 @@ def register(request):
 		if form.is_valid():
 			form.save()
 			username = form.cleaned_data.get('username')
-			raw_password = form.cleaned_data.get('password1')
-			user = authenticate(username=username, password=raw_password)
-			messages.success(request, 'Account created for {username}!')
+			password = form.cleaned_data.get('password1')
+			user = authenticate(username=form.cleaned_data.get('username'),password=form.cleaned_data['password1'])
+			#messages.success(request, 'Account created for {username}!')
 			#login(request, user)
-			return redirect('index')
+			return HttpResponseRedirect("http://127.0.0.1:8000/questionnaire/1")
 	else:
 		form = UserRegisterForm()
 	return render(request, 'registration/register.html', {'form': form})
 
 class login(LoginView):
 	template_name='registration/login.html'
+
