94f1d70b99362f8d871fce533638b731fad045d9
LeanderDavis <leander.davis@g.austincc.edu>
2019-04-09 12:09:54 -0400

    Page for adding friends.

--- SaltShaker/settings.py
+++ SaltShaker/settings.py
@@ -28,7 +28,7 @@ ALLOWED_HOSTS = ['localhost','127.0.0.1',]
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,8 +40,10 @@ INSTALLED_APPS = [
     'landing',
     'registration',
     'questionnaire',
-    'crispy_forms'
-]
+    'crispy_forms',
+    # 'friends',
+    'friendslist'
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
