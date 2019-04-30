306f8aec6066aadaec066843f8247fe56874807e
APRYM <37633613+APRYM@users.noreply.github.com>
2019-04-29 20:56:23 -0500

    personalized add friend buttons based on status

--- Users/views.py
+++ Users/views.py
@@ -10,14 +10,31 @@ from connect.models import Friend, FriendshipRequest
 @login_required(login_url='/login/')
 def profile(request, user_pk):
     profile = get_object_or_404(UserProfile, user = user_pk)
+    friends = Friend.objects.friends(request.user)
+    my_request = FriendshipRequest.objects.filter(to_user=request.user).values_list('from_user', flat=True).order_by('id')
+    sent_requests=FriendshipRequest.objects.filter(from_user=request.user).values_list('to_user', flat=True).order_by('id')
     
     if request.method == 'POST':
-        if 'addFriend' in request.POST:
-            sendRequest= request.POST.get("addFriend")
-            other = User.objects.get(pk=sendRequest)
+        if 'sendRequest' in request.POST:
+            sendRequest= request.POST.get("sendRequest")
+            other_user = User.objects.get(pk=sendRequest)
             Friend.objects.add_friend(
-                request.user,                               # The sender
-                other)
+                request.user,                              
+                other_user)
+        elif 'acceptRequest' in request.POST:
+            accept= request.POST.get("acceptRequest")
+            friend_request = FriendshipRequest.objects.get(from_user=accept)
+            friend_request.accept()
+        elif 'rejectRequest' in request.POST:
+            reject= request.POST.get("rejectRequest")
+            friend_request = FriendshipRequest.objects.get(from_user=reject)
+            friend_request.reject()
+        elif 'deleteFriend' in request.POST:
+            remove = request.POST.get("deleteFriend")
+            remove_user = User.objects.get(pk=remove);
+            Friend.objects.remove_friend(
+                request.user,                               
+                remove_user)
         ratingForm = UserRatingForm(request.POST)
         if ratingForm.is_valid():
             UserRating.objects.filter(userDoingTheRating=request.user).delete() #delete past rating/refresh response
@@ -30,10 +47,12 @@ def profile(request, user_pk):
             ratingInstance.save()
     else:
         ratingForm = UserRatingForm()
-		
     context = {
-        'profile':profile,
-        'ratingForm':ratingForm,
+            'profile':profile,
+            'ratingForm':ratingForm,
+            'friends': friends,
+            'my_request': my_request,
+            'sent_requests': sent_requests,
     }
 
     return render(request, 'User/Profile.html',context)
