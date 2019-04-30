3ff2917634a669e0a4492f54023c5048736f5f3a
APRYM <37633613+APRYM@users.noreply.github.com>
2019-04-29 19:03:01 -0500

    search bar removes friends + sent requests

---
+++ connect/connect/views.py
@@ -0,0 +1,55 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
+try:
+    from django.contrib.auth import get_user_model
+    user_model = get_user_model()
+except ImportError:
+    from django.contrib.auth.models import User
+    user_model = User
+from django.shortcuts import render, get_object_or_404, redirect
+
+from connect.exceptions import AlreadyExistsError
+from connect.models import Friend, FriendshipRequest
+from Users.models import UserProfile
+
+get_friendship_context_object_name = lambda: getattr(settings, 'FRIENDSHIP_CONTEXT_OBJECT_NAME', 'user')
+get_friendship_context_object_list_name = lambda: getattr(settings, 'FRIENDSHIP_CONTEXT_OBJECT_LIST_NAME', 'users')
+
+
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+	if request.method == "POST":
+		if 'sendRequest' in request.POST:
+			sendRequest= request.POST.get("sendRequest")
+			other_user = User.objects.get(pk=sendRequest)
+			Friend.objects.add_friend(
+    			request.user,                               # The sender
+    			other_user)
+			#friendship_add_friend(request, sendRequest)
+		elif 'acceptRequest' in request.POST:
+			accept= request.POST.get("acceptRequest")
+			friend_request = FriendshipRequest.objects.get(from_user=accept)
+			friend_request.accept()
+			#friendship_accept(request, accept)
+		elif 'rejectRequest' in request.POST:
+			reject= request.POST.get("rejectRequest")
+			friend_request = FriendshipRequest.objects.get(from_user=reject)
+			friend_request.reject()
+			#friendship_reject(request, reject)
+#			FriendRequests.objects.filter(user=request.user, request= myFriend).update(friendship=True)
+		return HttpResponseRedirect("/AddFriends")
+	else:
+		my_request = FriendshipRequest.objects.filter(to_user=request.user)
+		myuser = User.objects.all()
+		my_friends = Friend.objects.friends(request.user)
+		old_requests = FriendshipRequest.objects.filter(from_user=request.user).values_list('to_user', flat=True).order_by('id')
+		my_context = {
+			"my_request": my_request,
+			"myuser": myuser,
+			"my_friends" : my_friends,
+			"old_requests" : old_requests
+		}
+		return render(request, "connect/connect.html", my_context)
